4efabbeceb365c1706e6c2c9ccca36dcc576725a
Kleber <klebermeira4@gmail.com>
2021-12-22 10:25:18 -0400

    add agenda

---
+++ src/components/EventModal.js
@@ -0,0 +1,203 @@
+import React, { useContext, useState } from "react";
+import GlobalContext from "../context/GlobalContext";
+
+const labelsClasses = [
+  "indigo",
+  "gray",
+  "green",
+  "blue",
+  "red",
+  "purple",
+];
+
+export default function EventModal() {
+  const {
+    setShowEventModal,
+    daySelected,
+    dispatchCalEvent,
+    selectedEvent,
+  } = useContext(GlobalContext);
+
+  const [title, setTitle] = useState(
+    selectedEvent ? selectedEvent.title : ""
+  );
+  const [description, setDescription] = useState(
+    selectedEvent ? selectedEvent.description : ""
+  );
+  const [local, setLocal] = useState(
+    selectedEvent ? selectedEvent.local : ""
+  );
+  const [participantes, setParticipantes] = useState(
+    selectedEvent ? selectedEvent.participantes : ""
+  );
+  const [exclusivo, setExclusivo] = useState(
+    selectedEvent ? selectedEvent.exclusivo : ""
+  );
+
+  const [compartilhado, setCompartilhado] = useState(
+    selectedEvent ? selectedEvent.compartilhado : ""
+  );
+
+  //Nome, descrição, data, local e participantes;
+  const [selectedLabel, setSelectedLabel] = useState(
+    selectedEvent
+      ? labelsClasses.find((lbl) => lbl === selectedEvent.label)
+      : labelsClasses[0]
+  );
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const calendarEvent = {
+      title,
+      description,
+      local,
+      participantes,
+      exclusivo,
+      compartilhado,
+      label: selectedLabel,
+      day: daySelected.valueOf(),
+      id: selectedEvent ? selectedEvent.id : Date.now(),
+    };
+    console.log('Variaveis', calendarEvent);
+    if (selectedEvent) {
+      dispatchCalEvent({ type: "update", payload: calendarEvent });
+    } else {
+      dispatchCalEvent({ type: "push", payload: calendarEvent });
+    }
+
+
+
+    setShowEventModal(false);
+  }
+  return (
+    <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
+      <form className="bg-white rounded-lg shadow-2xl w-1/4">
+        <header className="bg-gray-100 px-4 py-2 flex justify-between items-center">
+          <span className="material-icons-outlined text-gray-400">
+            drag_handle
+          </span>
+          <div>
+            {selectedEvent && (
+              <span
+                onClick={() => {
+                  dispatchCalEvent({
+                    type: "delete",
+                    payload: selectedEvent,
+                  });
+                  setShowEventModal(false);
+                }}
+                className="material-icons-outlined text-gray-400 cursor-pointer"
+              >
+                delete
+              </span>
+            )}
+            <button onClick={() => setShowEventModal(false)}>
+              <span className="material-icons-outlined text-gray-400">
+                close
+              </span>
+            </button>
+          </div>
+        </header>
+        <div className="p-3">
+          <div className="grid grid-cols-1/5 items-end gap-y-7">
+            <div></div>
+            <input
+              type="text"
+              name="title"
+              placeholder="Add Titulo"
+              value={title}
+              required
+              className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <span className="material-icons-outlined text-gray-400">
+              schedule
+            </span>
+            <p>{daySelected.format("dddd, MMMM DD")}</p>
+            <span className="material-icons-outlined text-gray-400">
+              segment
+            </span>
+            <input
+              type="text"
+              name="description"
+              placeholder="Descricao"
+              value={description}
+              required
+              className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <span className="material-icons-outlined text-gray-400">
+              bookmark_border
+            </span>
+            
+            <input
+              type="text"
+              name="local"
+              placeholder="Local"
+              value={local}
+              required
+              className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => setLocal(e.target.value)}
+            />
+            <span className="material-icons-outlined text-gray-400">
+              bookmark_border
+            </span>
+            <input
+              type="text"
+              name="participantes"
+              placeholder="Participantes"
+              value={participantes}
+              required
+              className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => setParticipantes(e.target.value)}
+            />
+            <label> Exclusivo </label>
+            <input
+              type="checkbox"
+              value={exclusivo}
+              required
+              className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => setExclusivo(e.target.value)}
+            />
+            
+            <label> Compartilhado </label>
+            <input
+              type="checkbox"
+              value={exclusivo}
+              required
+              className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
+              onChange={(e) => setCompartilhado(e.target.value)}
+            />
+            <span className="material-icons-outlined text-gray-400">
+              bookmark_border
+            </span>
+            <div className="flex gap-x-2">
+              {labelsClasses.map((lblClass, i) => (
+                <span
+                  key={i}
+                  onClick={() => setSelectedLabel(lblClass)}
+                  className={`bg-${lblClass}-500 w-6 h-6 rounded-full flex items-center justify-center cursor-pointer`}
+                >
+                  {selectedLabel === lblClass && (
+                    <span className="material-icons-outlined text-white text-sm">
+                      check
+                    </span>
+                  )}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+        <footer className="flex justify-end border-t p-3 mt-5">
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded text-white"
+          >
+            Save
+          </button>
+        </footer>
+      </form>
+    </div>
+  );
+}
